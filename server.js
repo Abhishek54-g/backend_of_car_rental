ab1000bf48de050550c5f836ccf54703e79eec86
Abhishek54-g <abhi.shrivastava1872000@gmail.com>
2026-05-13 21:00:48 +0530

    Initialize Express server with routes and middleware
    Here this file is execute to start our Server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const carRoutes = require("./routes/carRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/cars", carRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/contact", contactRoutes);
+
+app.get("/", (req, res) => {
+res.send("Rental Car API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+})
