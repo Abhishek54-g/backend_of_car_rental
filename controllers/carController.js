31c3a2aac49a7e7fb4159ae4d9abee658ee1aa6d
Abhishek54-g <abhi.shrivastava1872000@gmail.com>
2026-05-13 21:23:42 +0530

    Implement CRUD operations for Car model
    This file is used to add new car, get car details, update status of car, delete car

---
+++ controllers/carController.js
@@ -0,0 +1,54 @@
+const Car = require("../models/Car");
+
+// Add Car
+exports.addCar = async (req, res) => {
+  try {
+    const car = await Car.create(req.body);
+    res.status(201).json(car);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get All Cars
+exports.getCars = async (req, res) => {
+  try {
+    const cars = await Car.find();
+    res.status(200).json(cars);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get Single Car
+exports.getCarById = async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.id);
+    if (!car) return res.status(404).json({ message: "Car not found" });
+    res.status(200).json(car);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Update Car
+exports.updateCar = async (req, res) => {
+  try {
+    const car = await Car.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(car);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Delete Car
+exports.deleteCar = async (req, res) => {
+  try {
+    await Car.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Car deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
