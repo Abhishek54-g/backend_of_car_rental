ec364718cecf4bae24f9320217404a1ae630f76a
Abhishek54-g <abhi.shrivastava1872000@gmail.com>
2026-05-13 21:33:42 +0530

    Create adminMiddleware.js
    middleware for admin

---
+++ middleware/adminMiddleware.js
@@ -0,0 +1,13 @@
+const adminOnly = (req, res, next) => {
+    try {
+      if (req.user && req.user.role === "admin") {
+        next();
+      } else {
+        return res.status(403).json({ message: "Access denied. Admin only." });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: "Server Error" });
+    }
+  };
+  
+  module.exports = adminOnly;
